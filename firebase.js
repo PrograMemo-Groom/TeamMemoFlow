20bd4cfafc4272f315cb4e6a59814b0f7d85c7f4
Jae-Hong-Kim <newfly101@naver.com>
2025-01-06 13:55:52 +0900

    🎉 프로젝트 기초 구성 (경로 재설정) refs: #1

--- firebase.js
+++ firebase.js
@@ -1,12 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import {
     FIREBASE_API_KEY, FIREBASE_APP_ID,
     FIREBASE_AUTH_DOMAIN,
     FIREBASE_MESSAGING_SENDER_ID,
     FIREBASE_PROJECT_ID,
     FIREBASE_STORAGE_BUCKET
-} from "./src/config/Config";
+} from "./src/config/Config.js";
 
 const firebaseConfig = {
     apiKey: FIREBASE_API_KEY,
