fcd5c41e21e2747e0df968039b54d36a0dc5980a
Jae-Hong-Kim <newfly101@naver.com>
2025-01-06 20:52:07 +0900

    🐛 react-router-dom warning 부분 해결 및 코드 수정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './reset.css';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <Router future={{ v7_startTransition: true, v7_relativeSplatPath:true }}>
     <App/>
-  </BrowserRouter>
+  </Router>
 );
