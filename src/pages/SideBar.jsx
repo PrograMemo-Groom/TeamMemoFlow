53df6bf9bdc26ca372f03c9bf7956dc753049b45
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2025-01-06 16:10:48 +0900

    ✨SideBar 기본 레이아웃 작업 REFS: #4

--- src/pages/SideBar.jsx
+++ src/pages/SideBar.jsx
@@ -1,7 +1,61 @@
+import styled from "styled-components";
+import { FiHome, FiUsers,FiSearch,FiSettings    } from "react-icons/fi";
+const Container = styled.div`
+    width: 260px;
+    height: 100vh;
+    background-color: #D9D9D9;
+
+`
+const MenuUl = styled.ul`
+    display: flex;
+    flex-direction: column;
+    padding-top: 20px;
+    width: 100%;
+`
+const MenuLi = styled.li`
+    height: 64px;
+    cursor: pointer;
+    display: flex;
+    padding-left: 22px;
+    align-items: center;
+    &:hover{
+        background-color: #9F9F9F;
+        border-radius: 8px;
+    }
+`
+const MenuIconContainer = styled.span`
+    width: 24px;
+    height: 24px;
+    
+    svg {
+        width: 24px;
+        height: 24px;
+    }
+`
+const MenuText = styled.span`
+    margin-left: 28px;
+    font-size: 24px;
+    font-weight: bold;
+`
+
+const MenuItem = [
+  {icon: <FiHome/>, text: 'Home'},
+  {icon: <FiUsers/>, text: 'Team'},
+  {icon: <FiSearch/>, text: 'Explore'},
+  {icon: <FiSettings/>, text: 'Setting'},
+]
+
 export default function SideBar(){
   return(
-    <div>
-
-    </div>
+    <Container>
+      <MenuUl>
+        {MenuItem.map((item,index) => (
+          <MenuLi key={index}>
+            <MenuIconContainer>{item.icon}</MenuIconContainer>
+            <MenuText>{item.text}</MenuText>
+          </MenuLi>
+        ))}
+      </MenuUl>
+    </Container>
   )
 }
