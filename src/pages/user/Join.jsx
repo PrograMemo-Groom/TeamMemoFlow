61f674cb2ad8b0034be94ae820eb9c5dffdbd260
Jae-Hong-Kim <newfly101@naver.com>
2025-01-07 15:46:23 +0900

    📝 회원가입 페이지 생성 REFS: #21

---
+++ src/pages/user/Join.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Join.module.scss";
+
+const Join = () => {
+    return (
+        <div className={styles.container}>
+            <h1>회원가입 페이지</h1>
+        </div>
+    );
+};
+
+export default Join;
