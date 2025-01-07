7aee0aff59084c4e9596b17c82589674377c9e16
Jae-Hong-Kim <newfly101@naver.com>
2025-01-07 16:17:00 +0900

    🎨 STORE에 로그인 정보 저장 CLOSE: #16

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -1,26 +1,57 @@
 import React from 'react';
 import styles from './Login.module.scss';
 import {useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {setUserLogin} from "../../store/slice/UserSlice";
 
 const Login = () => {
     const navigate = useNavigate();
+    const [userId, setUserId] = React.useState("");
+    const [userPw, setUserPw] = React.useState("");
+    const dispatch = useDispatch();
+
+    const handleChangeUserId = (id) => {
+        setUserId(id);
+    }
+    const handleChangeUserPw = (pw) => {
+        setUserPw(pw);
+    }
+    const handleLoginCheck = (e) => {
+        e.preventDefault();
+        dispatch(setUserLogin({id:userId,password:userPw}));
+        // console.log("userId", userId);
+        // console.log("userPw", userPw);
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
                 <h1 className={styles.title}>TeamMemoFlow</h1>
                 <h2 className={styles.subtitle}>간단설명 작성해주세요</h2>
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleLoginCheck}>
                     <div className={styles.inputWrapper}>
                         <label className={styles.label} htmlFor="userId">아이디</label>
-                        <input className={styles.input} id="userId" type="text" placeholder="아이디"/>
+                        <input className={styles.input}
+                               id="userId"
+                               type="text"
+                               placeholder="아이디"
+                               value={userId}
+                               onChange={(e) => handleChangeUserId(e.target.value)}
+                        />
                     </div>
                     <div className={styles.inputWrapper}>
                         <label className={styles.label} htmlFor="userPw">비밀번호</label>
-                        <input className={styles.input} id="userPw" type="password" placeholder="비밀번호"/>
+                        <input className={styles.input}
+                               id="userPw"
+                               type="password"
+                               placeholder="비밀번호"
+                               value={userPw}
+                               onChange={(e) => handleChangeUserPw(e.target.value)}
+                        />
                     </div>
                     <label className={styles.findPw} onClick={() => navigate("/findpwd")}>비밀번호를 잊으셨나요?</label>
-                    <button className={styles.loginButton}>로그인</button>
-                    <button className={styles.joinButton} onClick={() => navigate("/join")}>회원가입</button>
+                    <button className={styles.loginButton} type="submit">로그인</button>
+                    <button className={styles.joinButton} type="button" onClick={() => navigate("/join")}>회원가입</button>
                 </form>
             </div>
 
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,9 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import commonReducer from "./slice/CommonSlice";
+import userReducer from "./slice/UserSlice";
 
 const store = configureStore({
     reducer: {
-        // user: userReducer,
+        user: userReducer,
         common: commonReducer,
     }
 })
