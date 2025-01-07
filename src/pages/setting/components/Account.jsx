0b10883399e19a76dacb90ca934ba908d15b49d7
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2025-01-07 16:05:30 +0900

    📝 세팅 페이지 Account 컴포넌트 생성 및 ui 구성  ref: #57

---
+++ src/pages/setting/components/Account.jsx
@@ -0,0 +1,39 @@
+import styles from "./Account.module.scss"
+import {useState} from "react";
+
+
+const Account = () =>{
+    const [selectedOption, setSelectedOption] = useState("");
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
+    return(
+        <fieldset>
+            <legend>계정</legend>
+            <input className={styles.commonInput} placeholder="현재 이메일 주소"/>
+            <input className={styles.commonInput} placeholder="현재 비밀번호"/>
+            <form>
+                <select className={styles.gender} value={selectedOption} onChange={handleChange}>
+                    <option className={styles.defaultOption} value="" disabled>
+                      성별
+                    </option>
+                    <option value="option1">남자</option>
+                    <option value="option2">여자</option>
+                </select>
+                <select className={styles.birth} value={selectedOption} onChange={handleChange}>
+                    <option className={styles.defaultOption} value="" disabled>
+                       생년월일
+                    </option>
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                </select>
+            </form>
+            <input className={styles.commonInput} placeholder="핸드폰 번호"/>
+        </fieldset>
+    )
+}
+
+export default Account;
