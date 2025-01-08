038bfc6d1b0b5e687a1c97a9407a45a67fcd9921
Jae-Hong-Kim <newfly101@naver.com>
2025-01-08 10:42:56 +0900

    💄 회원가입 페이지 UI 구현 REF: #21

--- src/pages/user/Join.jsx
+++ src/pages/user/Join.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
-import styles from "./Join.module.scss";
+import styles from "./Login.module.scss";
 
 const Join = () => {
+    const [user, setUser] = React.useState({
+        id: "",
+        password: "",
+        name: "",
+        email: "",
+    });
+
+    const handleChangeUserInfo = (value, tag="") => {
+        // object 안에 key를 check해서 해당 값인 경우에만 값 변경 넣어줌
+        const validOption = Object.keys(user).includes(tag);
+        if (validOption) {
+            console.log(validOption, value, tag);
+            setUser((state) => ({
+                ...state,
+                [tag]: value
+            }));
+        }
+
+    }
+    const handleJoinUser = (e) => {
+        e.preventDefault();
+    }
     return (
         <div className={styles.container}>
-            <h1>회원가입 페이지</h1>
+            <div className={styles.wrapper}>
+                <form className={styles.form} onSubmit={handleJoinUser}>
+                    <div className={styles.inputWrapper}>
+                        <label className={styles.label} htmlFor="userId">아이디</label>
+                        <input className={styles.input}
+                               id="userId"
+                               type="text"
+                               placeholder="아이디"
+                               value={user.id}
+                               onChange={(e) => handleChangeUserInfo(e.target.value, "id")}
+                        />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
